0d1a10e9a7dd74efb837ec92ca60b9b8cfeba285
kurdyukov <leonid.kurdyukov@icloud.com>
2024-09-16 17:05:36 +0300

    Диаграмма сиборн

--- graph_sns.py
+++ graph_sns.py
@@ -1,23 +1,23 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Данные
+# прописываем данные графика
 labels = ['Иваново', 'Кохма', 'Шуя', 'Лежнево', 'Савино']
 values = [11.76, 5.88, 35.29, 23.53, 23.53]
 
-# Настройка стиля Seaborn
+# настраиваем стиль
 sns.set(style="darkgrid")
 
-# Создание графика
+# создаем график
 plt.figure(figsize=(10, 6))
 barplot = sns.barplot(x=labels, y=values, palette='colorblind', edgecolor='black')
 
-# Настройка заголовка и меток
-plt.title('Данные по городам', fontsize=16, fontweight='bold')
-plt.xlabel('Города', fontsize=14, fontweight='bold')
-plt.ylabel('Значения', fontsize=14)
+# делаем настройки графика
+plt.title('Количество СОЧ по муниципальным образованиям', fontsize=16, fontweight='bold')
+plt.xlabel('Муниципальные образования', fontsize=14, fontweight='bold')
+plt.ylabel('Проценты', fontsize=14)
 plt.xticks(rotation=15, fontsize=12)
 
-# Показать график
-plt.tight_layout()  # Автоматическая подстройка параметров
+# выводим график
+plt.tight_layout()
 plt.show()
